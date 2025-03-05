915fc43f490745028ea6c5cedcd02a570852a7dd
AlPakh <84967883+AlPakh@users.noreply.github.com>
2025-03-06 00:50:09 +0300

    Update db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("postgresql://topotik_test_db_user:tUNRiVqHiwDGIz2SyqhGcJQYIezYJZKz@dpg-cv4btiggph6c738ub4a0-a/topotik_test_db")  # Переменная для подключения к базе
+DATABASE_URL = os.getenv("postgresql://topotik_test_db_user:tUNRiVqHiwDGIz2SyqhGcJQYIezYJZKz@dpg-cv4btiggph6c738ub4a0-a.frankfurt-postgres.render.com/topotik_test_db")  # Переменная для подключения к базе
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
